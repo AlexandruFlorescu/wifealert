68d8fe0045ec16b2a0f7d410b6ab4aecfb219d5b
Alexandru Florescu <acdc152@gmail.com>
2018-07-19 20:27:09 +0300

    wifealert

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { NavController, Platform } from 'ionic-angular';
+import { Geofence, Geolocation, SMS } from 'ionic-native';
+import { ActivePage } from '../active/active';
 
 @Component({
   selector: 'page-home',
@@ -7,8 +9,51 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  radius: number = 100;
+  error: any;
+  success: any;
 
+  constructor(public navCtrl: NavController, private platform: Platform) {
+    this.platform.ready().then(() => {
+      Geofence.initialize().then(
+        () => console.log('Geofence Plugin Ready'),
+        (err) => console.log(err)
+      );
+    });
   }
+
+  setGeofence(value: number) {
+
+    Geolocation.getCurrentPosition({
+      enableHighAccuracy: true
+    }).then((resp) => {
+      var longitude = resp.coords.longitude;
+      var latitude = resp.coords.latitude;
+      var radius = value;
+
+      let fence = {
+        id: 'myGeofenceID1',
+        latitude: latitude,
+        longitude: longitude,
+        radius: radius,
+        transitionType: 2
+      }
+
+      Geofence.addOrUpdate(fence).then(
+        () => this.success = true,
+        (err) => this.error = "failed with error" + err.message
+      );
+
+    Geofence.onTransitionReceived().subscribe(resp=>{
+        SMS.send('0773970389', 'A mers!');
+
+    });
+
+    this.navCtrl.push(ActivePage);
+}).catch((error)=>{
+    this.error = error;
+})
+
+}
 
 }
